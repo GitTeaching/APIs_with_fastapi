aa67dcdd228b72e092279966653d683e24555e75
Aicha Aid <aid_aicha@hotmail.fr>
2021-02-08 13:42:42 +0100

    Complete python_microservices added

---
+++ devdotto_microservices/python_microservices/cast-service/app/api/db.py
@@ -0,0 +1,22 @@
+import os
+
+from sqlalchemy import (Column, Integer, MetaData, String, Table,
+                        create_engine, ARRAY)
+
+from databases import Database
+
+#DATABASE_URI = os.getenv('DATABASE_URI')
+DATABASE_URI = 'postgresql://postgres:admin@localhost/cast_db'
+
+engine = create_engine(DATABASE_URI)
+metadata = MetaData()
+
+casts = Table(
+    'casts',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('nationality', String(20)),
+)
+
+database = Database(DATABASE_URI)
